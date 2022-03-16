a7d128f6c92d2d2ee658df3b78540f3c7e20c2b7
Paulina <hi@zybinska.io>
2022-03-16 09:52:10 +0100

    Arduino New

--- Day 2/Exercise4/Exercise4.ino
+++ Day 2/Exercise4/Exercise4.ino
@@ -5,10 +5,13 @@ int inputVal = 0;
 
 void setup() {
   pinMode(LED, OUTPUT);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
 }
 
 void loop() {
   inputVal = analogRead(potmeter);
+  Serial.println(inputVal);
   analogWrite(LED, inputVal/4);
   delay(100);
 }
